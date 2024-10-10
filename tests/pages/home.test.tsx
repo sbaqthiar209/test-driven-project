80a1e147eb2bd301396cdb012bbf1b27676f6f86
Sheik Baqthiar <sheik.b@like.digital>
2024-10-10 16:54:06 +0530

    added test case to check controlled component sanity, test failing

--- tests/pages/home.test.tsx
+++ tests/pages/home.test.tsx
@@ -8,6 +8,7 @@ import StringCalculator from '../../src/pages/home';
 import userEvent from '@testing-library/user-event';
 
 describe('String Calculator Component Test', () => {
+  const testInputString = "1,2,3"
 
   it('Check if String Calculator UI is rendered', () => {
     render(
@@ -15,5 +16,14 @@ describe('String Calculator Component Test', () => {
     );
     const inputElement = screen.getByTestId("input-element");
     expect(inputElement).toBeInTheDocument();
+  });
+
+  it('User will type text in the input and test if controlled component is working fine using React state', async () => {
+    render(
+      <StringCalculator />
+    );
+    const inputElement = screen.getByTestId("input-element");
+    await userEvent.type(inputElement,testInputString)
+    expect(inputElement).toHaveValue(testInputString);
   });
 });
