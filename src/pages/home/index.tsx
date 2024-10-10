743c2837e8f8e804f09c0a362913eae11fdb184e
Sheik Baqthiar <sheik.b@like.digital>
2024-10-10 16:35:08 +0530

    test passed, by rendering an input element in the UI

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,22 @@
-import { Box } from "@mui/material";
+import { Box, TextField, Typography ,Button} from "@mui/material";
 import styles from "./home.module.scss";
 
 const StringCalculator = () => {
+  const handleInputChange = (newValue: string) => {
+    console.log("newValue is-", newValue);
+  };
   return (
     <>
-      <Box className={styles.homeContainer}>
-        Test Page
+      <Box className={styles.calculatorContainer}>
+        <Typography variant="body1" title={"Help text"}>Type text below</Typography>
+        <TextField
+          variant="outlined"
+          onChange={(e) =>
+            handleInputChange((e.target as HTMLInputElement).value)
+          }
+          data-testid={"input-element"}
+        />
+        <Button variant="contained">Add</Button>
       </Box>
     </>
   );
