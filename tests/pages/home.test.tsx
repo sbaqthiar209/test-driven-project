e7ec4d9a3f33e4052b2cefa7debff9ef2e29f025
Sheik Baqthiar <sheik.b@like.digital>
2024-10-11 09:58:01 +0530

    Test case failed - Step2 : added test case to accept any amount of number

--- tests/pages/home.test.tsx
+++ tests/pages/home.test.tsx
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
@@ -19,20 +18,18 @@ describe("String Calculator Component Test", () => {
   });
 
   it("User will type text in the input and test if controlled component is working fine using React state", async () => {
-    inputElement = screen.getByTestId("input-element");
     await userEvent.type(inputElement, testInputString);
     expect(inputElement).toHaveValue(testInputString);
   });
 
   it("Check if Add button is working with correctly", async () => {
-    inputElement = screen.getByTestId("input-element");
     await userEvent.type(inputElement, testInputString);
     expect(inputElement).toHaveValue(testInputString);
   });
 });
 
 describe("Assessment test cases for String calculator", () => {
-  const testInputString = "1,2,3";
+  const shortInputString = "1,2,3" ;//sum=6
   let inputElement: HTMLInputElement;
   beforeEach(() => {
     render(<StringCalculator />);
@@ -40,11 +37,18 @@ describe("Assessment test cases for String calculator", () => {
   });
 
   it("Check if Add is adding the comma separated digits correctly", async () => {
-    inputElement = screen.getByTestId("input-element");
-    await userEvent.type(inputElement, testInputString);
-    expect(inputElement).toHaveValue(testInputString);
+    await userEvent.type(inputElement, shortInputString);
+    expect(inputElement).toHaveValue(shortInputString);
     const buttonElement = screen.getByTestId("add-button");
     await userEvent.click(buttonElement);
     expect(inputElement).toHaveValue("6");
+  });
+  it("To provide any amount of numbers in the input (More than 7 character string)", async () => {
+    const longInputString = "2,1,2,1,2,1,2,1,2,1,2,1"; //sum=18
+    await userEvent.type(inputElement, longInputString);
+    expect(inputElement).toHaveValue(longInputString);
+    const buttonElement = screen.getByTestId("add-button");
+    await userEvent.click(buttonElement);
+    expect(inputElement).toHaveValue("18");
   });
 });
